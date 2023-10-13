b00b51c757c329e4cf88104b5bd6b3f5c07e8456
mirza <mirzaramadhan1304@gmail.com>
2023-10-13 10:42:13 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "---------------------------------------" << endl;
+    cout << "Nama : Mirza Ramadhan" << endl << endl;//Identitas Diri
+    cout << "NPM : 2310631170139" <<endl << endl;
+    cout << "Kelas : 1A" << endl << endl;
+    cout << "Asal : Bekasi" << endl << endl;
+    cout << "Cita Cita : To Be FullStack Developer" << endl;
+    cout << "---------------------------------------" <<endl;
+    return 1;
+}
